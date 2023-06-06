9c3edc9a6def1e1b5488d07fcc2dac09f62822d1
irtazaakram <irtaza.akram@arbisoft.com>
2023-06-06 16:53:47 +0500

    feat: Add support for django 3 to 4

--- fernet_fields/test/test_fields.py
+++ fernet_fields/test/test_fields.py
@@ -3,7 +3,7 @@ from datetime import date, datetime
 
 from django.core.exceptions import FieldError, ImproperlyConfigured
 from django.db import connection, models as dj_models
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 import pytest
 
 import fernet_fields as fields
@@ -79,7 +79,7 @@ class TestEncryptedFieldQueries(object):
         with connection.cursor() as cur:
             cur.execute('SELECT value FROM %s' % model._meta.db_table)
             data = [
-                force_text(field.fernet.decrypt(force_bytes(r[0])))
+                force_str(field.fernet.decrypt(force_bytes(r[0])))
                 for r in cur.fetchall()
             ]
 
